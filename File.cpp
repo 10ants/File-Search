f952dfffc77d036733457e2d1e8bddddfcb06bb9
Jesse Tennant <75157270+BeepBwop@users.noreply.github.com>
2022-03-09 15:32:36 -0800

    Update File.cpp

--- File.cpp
+++ File.cpp
@@ -4,45 +4,44 @@
 #include <sstream>
 using namespace std;
 
-
 int main()
 {
     string myFile = "";
     cout << "Enter the name of the file you would like to access, or enter XXXX to quit:";
     cin >> myFile;
 
-    while (myFile!= "XXXX") // while quit is not wanted
+    while (myFile!= "XXXX") // While not quit
     {
         ofstream output;
 
-        output.open(myFile);
+        output.open(myFile); // Opening file asked for 
       
-        cout << "Enter text: "; // Enter text to be added to file
-        string text;
+        cout << "Enter text: "; //Gathering user input for file
+        string NewText;
    
         cin.ignore(); // flush buffer
-        std::getline(std::cin, text);
+        std::getline(std::cin, NewText);
        
-        output << text << endl;; // inputting new text to file
-        output.close();
+        output << NewText << endl;; // inputting new text to file
+        output.close(); // Closing file
 
         cout << "Enter the forbidden word: ";
         string forbidden;
         cin >> forbidden;
-        fstream editedString;
+        fstream editedString; // creating edited string that will be outputted
       
-        editedString.open(myFile); // reading file
+        editedString.open(myFile); // openning filed asked for, with new string
         if (editedString.is_open()) {//if the file is open
-            string temp;
+            string tString;
             
-            while (getline(editedString, temp)) 
+            while (getline(editedString, tString)) 
             {
-                stringstream stream(temp);
+                stringstream stream(tString);
                 
                 while (stream.good()) 
                 {
                     string newString;
-                    getline(stream, newString, ' ');
+                    getline(tString, newString, ' ');
                     
                     if (newString.compare(forbidden) != 0)
                         cout << newString << " ";
